6465034fab761f829d71ab201b9cd4476fe08c24
Code-Hashirah <kabirajibade@yahoo.com>
2023-07-29 12:34:35 +0100

    Done with adding product, next is fetching

--- addproduct.php
+++ addproduct.php
@@ -1,5 +1,75 @@
 <?php 
+require_once "database.php";
+$category=[];
+$error=[];
+$product=[];
+if(isset($_POST['Add'])){
+    // for item 
+    if(empty($_POST['Item'])){
+        $error['itemErr']="This field is required *";
+    }
+    else{
+        $product['Item']=$_POST['Item'];
+    }
 
+      // for Price 
+    if(empty($_POST['Price'])){
+        $error['priceErr']="This field is required *";
+    }
+    else{
+        $product['Price']=$_POST['Price'];
+    }
+
+      // for Description 
+    if(empty($_POST['Description'])){
+        $error['descriptionErr']="This field is required *";
+    }
+    else{
+        $product['Description']=$_POST['Description'];
+    }
+
+      // for Description 
+      if(empty($_POST['Image'])){
+        $error['imageErr']="This field is required *";
+    }
+    else{
+        $product['Image']=$_POST['Image'];
+    }
+
+      // for Discount 
+      if(empty($_POST['Discount'])){
+        $error['discountErr']="This field is required *";
+    }
+    else{
+        $product['Discount']=$_POST['Discount'];
+    }
+
+      // for Category 
+      if(empty($_POST['Category'])){
+        $error['categoryErr']="This field is required *";
+    }
+    else{
+        $product['Category']=$_POST['Category'];
+    }
+
+    // simplifying inputs 
+$item=$_POST['Item'];
+$price=$_POST['Price'];
+$description=$_POST['Description'];
+$image=$_POST['Image'];
+$discount=$_POST['Discount'];
+$category=implode(" ",$_POST['Category']);
+
+if(count($error)==0){
+    $sqlcommand="INSERT INTO products(item,price,description,image,discount,category) VALUES('$item','$price','$description','$image','$discount','$category')";
+    if(mysqli_query($connection, $sqlcommand)){
+        echo "Product saved successfully";
+    }
+}
+else{
+    echo "Unable to save product";
+}
+}
 
 ?>
 <!DOCTYPE html>
@@ -13,41 +83,73 @@
     <title>Add Product</title>
 </head>
 <body>
-    <h2 id="header">Add Products</h2>
+
+    <header>
+        
+    </header>
+
+  <main id="mainbox">
+  <h2 id="header">Add Products</h2>
    <div class="container-fliud" id="form-box">
         <form action="addproduct.php" id="main-form" method="post">
             <div>
                 <label for="" class="lbl">Item</label>
                 <input type="text" class="inputTxt" name="Item">
+                <p class="text-danger">
+                    <?php echo array_key_exists('itemErr',$error)? $error['itemErr']: " " ?>
+                </p>
             </div>
             <div>
                 <label for="" class="lbl">Price</label>
                 <input type="text" class="inputTxt" name="Price">
+                <p class="text-danger">
+                <?php echo array_key_exists('priceErr', $error)? $error['priceErr']: " " ?>
+                </p>
             </div>
             <div>
                 <label for="" class="lbl">Description</label>
                 <input type="text" class="inputTxt" name="Description">
+                <p class="text-danger">
+                    <?php echo array_key_exists('descriptionErr', $error)? $error['descriptionErr'] : " " ?>       
+                </p>
             </div>
             <div>
                 <label for="" class="lbl">Image link</label>
                 <input type="text" class="inputTxt" name="Image">
+                <p class="text-danger">
+                   <?php echo array_key_exists('imageErr', $error)? $error['imageErr']: " " ?> 
+                </p>
             </div>
             <div>
                 <label for="" class="lbl">Discount</label>
-                <input type="text" class="inputTxt" name="Discount">
+                <input type="number" class="inputTxt" name="Discount">
+                <p class="text-danger">
+                <?php echo array_key_exists('discountErr',$error)? $error['discountErr']:" " ?>    
+                </p>
             </div>
             <div>
                 <label for="" class="lbl">Category</label> <br>
-              <span class="span">Electronics</span>  <input type="checkbox" class="inputChk" name="Category" value="Electronics">
-              <span class="span">Accessories</span> <input type="checkbox" class="inputChk" name="Category" value="Accessories">
-              <span class="span">Wears</span>    <input type="checkbox" class="inputChk" name="Category" value="Wears">
-              <span class="span">Food</span>   <input type="checkbox" class="inputChk" name="Category" value="Food">
+              <span class="span">Electronics</span>  <input type="checkbox" class="inputChk" name="Category[]" value="Electronics">
+              <span class="span">Accessories</span> <input type="checkbox"checked class="inputChk" name="Category[]" value="Accessories">
+              <span class="span">Wears</span>    <input type="checkbox" class="inputChk" name="Category[]" value="Wears">
+              <span class="span">Food</span>   <input type="checkbox" class="inputChk" name="Category[]" value="Food">
+              <span class="span">Smart Phone</span>   <input type="checkbox" class="inputChk" name="Category[]" value="Smart Phone">
+              <span class="span">Computer</span>   <input type="checkbox" class="inputChk" name="Category[]" value="Computer">
+              <p class="text-danger">
+                <?php echo array_key_exists('categoryErr',$error)? $error['categoryErr'] : " " ?>
+                    </p>
             </div>
             <div>
-                <button name="Add" type="submit" class="btn btn-outline-success">Add Product</button>
+                <button name="Add" type="submit" class="mx-4 btn btn-info">Add Product</button>
+                <button type="reset" class="mx-4 btn btn-warning">Clear</button>
             </div>
         </form>
    </div> 
+  </main>
+
+  <footer>
+
+  </footer>
 <!-- botstrap javascript  -->
 <script src="css/boostrap.bundle.min.js"></script>
 </body>
--- database.php
+++ database.php
@@ -1,4 +1,11 @@
 <?php 
+define("server","127.0.0.1");
+define("user","root");
+define("database",'ecommercedb');
+define("password", '');
 
-
+$connection=mysqli_connect(server,user,password,database);
+if(!$connection){
+    echo "Unable to establish connection to databse".mysqli_connect_error($connection);
+}
 ?>
